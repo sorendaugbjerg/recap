684b9a6faae70d8361c893ca5cc6fa81b52d4fb0
Søren Daugbjerg <sorendaugbjerg@me.com>
2022-08-01 15:59:05 +0200

    Re-add HTMLString

--- Sources/google-recaptcha-v2-ios/HTMLString/HTMLString.swift
+++ Sources/google-recaptcha-v2-ios/HTMLString/HTMLString.swift
@@ -10,10 +10,15 @@ public let htmlString = #"""
   <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <script src="https://www.google.com/recaptcha/api.js?onload=onLoad&render=explicit" async defer></script>
+    <script src="https://www.google.com/recaptcha/api.js?onload=onLoad&render=explicit&hl=en" async defer></script>
+    <title></title>
     <script type="text/javascript">
       const post = function(value) {
           window.webkit.messageHandlers.recaptcha.postMessage(value);
+      };
+
+      console.log = function(message) {
+          post(message);
       };
 
       var onLoad = function() {
@@ -21,8 +26,8 @@ public let htmlString = #"""
             "recaptcha",
             {
               sitekey: "${siteKey}",
-              callback: function(secret) {
-                  post(secret);
+              callback: function(token) {
+                  post(token);
               },
               size: "normal"
             }
